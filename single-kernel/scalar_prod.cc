6c0ddb55239e1c755d86fd0ee7dad4172cb1bf63
Sohan Lal <sohan.lal@tu-berlin.de>
2020-04-03 16:41:52 +0200

    revoved scalar product hierarchical

--- single-kernel/scalar_prod.cc
+++ single-kernel/scalar_prod.cc
@@ -8,17 +8,13 @@
 //using namespace cl::sycl;
 namespace s = cl::sycl;
 
-template<typename T, bool>
+template<typename T>
 class ScalarProdKernel;
-template<typename T, bool>
-class ScalarProdKernelHierarchical;
 
-template<typename T, bool>
+template<typename T>
 class ScalarProdReduction;
-template<typename T, bool>
-class ScalarProdReductionHierarchical;
 
-template<typename T, bool Use_ndrange = true>
+template<typename T>
 class ScalarProdBench
 {
 protected:    
@@ -65,29 +61,14 @@ public:
       // Use discard_write here, otherwise the content of the hostbuffer must first be copied to device
       auto intermediate_product = c.template get_access<s::access::mode::discard_write>(cgh, celerity::access::one_to_one<1>());
 
-      if(Use_ndrange){
-        cl::sycl::nd_range<1> ndrange (args.problem_size, args.local_size);
+      cl::sycl::nd_range<1> ndrange (args.problem_size, args.local_size);
 
-        cgh.parallel_for<class ScalarProdKernel<T, Use_ndrange>>(ndrange,
-          [=](cl::sycl::nd_item<1> item) 
-          {
-            size_t gid= item.get_global_linear_id();
-            intermediate_product[gid] = in1[gid] * in2[gid];
-          });
-      }
-      else {
-        #ifdef parallelForWorkGroup
-        cgh.parallel_for_work_group<class ScalarProdKernelHierarchical<T, Use_ndrange>>(
-          cl::sycl::range<1>{args.problem_size / args.local_size},
-          cl::sycl::range<1>{args.local_size},
-          [=](cl::sycl::group<1> grp){
-            grp.parallel_for_work_item([&](cl::sycl::h_item<1> idx){
-              size_t gid = idx.get_global_id(0);
-              intermediate_product[gid] = in1[gid] * in2[gid];
-            });
-          });
-        #endif
-      }
+      cgh.parallel_for<class ScalarProdKernel<T>>(ndrange,
+        [=](cl::sycl::nd_item<1> item) 
+        {
+          size_t gid= item.get_global_linear_id();
+          intermediate_product[gid] = in1[gid] * in2[gid];
+        });
     });
 
     // std::cout << "Multiplication of vectors completed" << std::endl;
@@ -108,74 +89,36 @@ public:
           auto local_mem = s::accessor <T, 1, s::access::mode::read_write, s::access::target::local> {s::range<1>(wgroup_size), cgh, celerity::access::one_to_one<1>()};
           cl::sycl::nd_range<1> ndrange (n_wgroups*wgroup_size, wgroup_size);
     
-          if(Use_ndrange) {
-            cgh.parallel_for<class ScalarProdReduction<T, Use_ndrange>>(ndrange,
-            [=](cl::sycl::nd_item<1> item) 
-              {
-                size_t gid= item.get_global_linear_id();
-                size_t lid = item.get_local_linear_id();
 
-                // initialize local memory to 0
-                local_mem[lid] = 0; 
+          cgh.parallel_for<class ScalarProdReduction<T>>(ndrange,
+          [=](cl::sycl::nd_item<1> item) 
+            {
+              size_t gid= item.get_global_linear_id();
+              size_t lid = item.get_local_linear_id();
 
-                if ((elements_per_thread * gid) < array_size) {
-                    local_mem[lid] = global_mem[elements_per_thread*gid] + global_mem[elements_per_thread*gid + 1];
+              // initialize local memory to 0
+              local_mem[lid] = 0; 
+
+              if ((elements_per_thread * gid) < array_size) {
+                  local_mem[lid] = global_mem[elements_per_thread*gid] + global_mem[elements_per_thread*gid + 1];
+              }
+
+              item.barrier(s::access::fence_space::local_space);
+
+              for (size_t stride = 1; stride < wgroup_size; stride *= elements_per_thread) {
+                auto local_mem_index = elements_per_thread * stride * lid;
+                if (local_mem_index < wgroup_size) {
+                    local_mem[local_mem_index] = local_mem[local_mem_index] + local_mem[local_mem_index + stride];
                 }
 
                 item.barrier(s::access::fence_space::local_space);
+              }
 
-                for (size_t stride = 1; stride < wgroup_size; stride *= elements_per_thread) {
-                  auto local_mem_index = elements_per_thread * stride * lid;
-                  if (local_mem_index < wgroup_size) {
-                      local_mem[local_mem_index] = local_mem[local_mem_index] + local_mem[local_mem_index + stride];
-                  }
-
-                  item.barrier(s::access::fence_space::local_space);
-                }
-
-                // Only one work-item per work group writes to global memory 
-                if (lid == 0) {
-                  global_mem[item.get_group_linear_id()] = local_mem[0];
-                }
-              });
-          }
-          else {
-            #ifdef parallelForWorkGroup
-            cgh.parallel_for_work_group<class ScalarProdReductionHierarchical<T, Use_ndrange>>(
-              cl::sycl::range<1>{n_wgroups}, cl::sycl::range<1>{wgroup_size},
-              [=](cl::sycl::group<1> grp){
-                grp.parallel_for_work_item([&](cl::sycl::h_item<1> idx){
-                  const size_t gid = idx.get_global_id(0);
-                  const size_t lid = idx.get_local_id(0);
-
-                  // initialize local memory to 0
-                  local_mem[lid] = 0; 
-
-                  if ((elements_per_thread * gid) < array_size) {
-                    local_mem[lid] = global_mem[elements_per_thread * gid] +
-                                     global_mem[elements_per_thread * gid + 1];
-                  }
-                });
-                for (size_t stride = 1; stride < wgroup_size; stride *= elements_per_thread) {
-                  grp.parallel_for_work_item([&](cl::sycl::h_item<1> idx){
-                  
-                    const size_t lid = idx.get_local_id(0);
-                    auto local_mem_index = elements_per_thread * stride * lid;
-
-                    if (local_mem_index < wgroup_size) {
-                      local_mem[local_mem_index] =
-                          local_mem[local_mem_index] +
-                          local_mem[local_mem_index + stride];
-                    }
-                  });
-                }
-                grp.parallel_for_work_item([&](cl::sycl::h_item<1> idx){
-                  const size_t lid = idx.get_local_id(0);
-                  global_mem[grp.get_id(0)] = local_mem[0];
-                });
-              });
-              #endif
-          }
+              // Only one work-item per work group writes to global memory 
+              if (lid == 0) {
+                global_mem[item.get_group_linear_id()] = local_mem[0];
+              }
+            });
         });
 
       array_size = n_wgroups;
@@ -208,7 +151,7 @@ public:
   static std::string getBenchmarkName() {
     std::stringstream name;
     name << "ScalarProduct_";
-    name << (Use_ndrange ? "NDRange_" : "Hierarchical_");
+   // name << (Use_ndrange ? "NDRange_" : "Hierarchical_");
     name << ReadableTypename<T>::name;
     return name.str();
   }
@@ -217,17 +160,10 @@ public:
 int main(int argc, char** argv)
 {
   BenchmarkApp app(argc, argv);
-  if(app.shouldRunNDRangeKernels()) {
-    app.run<ScalarProdBench<int, true>>();
-    app.run<ScalarProdBench<long long, true>>();
-    app.run<ScalarProdBench<float, true>>();
-    app.run<ScalarProdBench<double, true>>();
-  }
-
-  app.run<ScalarProdBench<int, false>>();
-  app.run<ScalarProdBench<long long, false>>();
-  app.run<ScalarProdBench<float, false>>();
-  app.run<ScalarProdBench<double, false>>();  
+  app.run<ScalarProdBench<int>>();
+  app.run<ScalarProdBench<long long>>();
+  app.run<ScalarProdBench<float>>();
+  app.run<ScalarProdBench<double>>();
 
   return 0;
 }
