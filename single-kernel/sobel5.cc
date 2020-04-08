463065612e1d6a89ae5bc6ac0914ce46f8d35930
Sohan Lal <sohan.lal@tu-berlin.de>
2020-04-08 13:07:06 +0200

    ported sobel 5x5 and 7x7

--- single-kernel/sobel5.cc
+++ single-kernel/sobel5.cc
@@ -33,18 +33,22 @@ public:
   void setup() {
     size = args.problem_size; // input size defined by the user
     input.resize(size * size);
-    load_bitmap_mirrored("../../share/Brommy.bmp", size, input);
+    load_bitmap_mirrored("../Brommy.bmp", size, input);
     output.resize(size * size);
 
-    input_buf.initialize(args.device_queue, input.data(), s::range<2>(size, size));
-    output_buf.initialize(args.device_queue, output.data(), s::range<2>(size, size));
+    input_buf.initialize(input.data(), s::range<2>(size, size));
+    output_buf.initialize(output.data(), s::range<2>(size, size));
   }
 
-  void run(std::vector<cl::sycl::event>& events) {
-    events.push_back(args.device_queue.submit(
-        [&](cl::sycl::handler& cgh) {
-      auto in  = input_buf .get_access<s::access::mode::read>(cgh);
-      auto out = output_buf.get_access<s::access::mode::discard_write>(cgh);
+  void run() {
+    celerity::distr_queue& queue = QueueManager::getInstance();
+
+    celerity::buffer<cl::sycl::float4,2>& a = input_buf.get();
+    celerity::buffer<cl::sycl::float4,2>& c = output_buf.get();
+
+    submit([=](celerity::handler& cgh) {
+      auto in  = a.get_access<s::access::mode::read>(cgh, celerity::access::one_to_one<2>());
+      auto out = c.get_access<s::access::mode::discard_write>(cgh, celerity::access::one_to_one<2>());
       cl::sycl::range<2> ndrange {size, size};
 
       // Sobel kernel 5x5
@@ -101,50 +105,62 @@ public:
           out[gid] = clamp(color, minval, maxval);
       }
        );
-     }));
+     });
    }
       
   bool verify(VerificationSetting &ver) {
-    // Triggers writeback
-    output_buf.reset();
-    save_bitmap("sobel5.bmp", size, output);
-
-    const float kernel[] = { 1, 2, 0,  -2, -1,4,  8, 0,  -8, -4, 6, 12, 0, -12, -6, 4,  8, 0,  -8, -4, 1,  2, 0,  -2, -1 };
-
     bool pass = true;
-    int radius = 5;
-    for(size_t i=ver.begin[0]; i<ver.begin[0]+ver.range[0]; i++){
-      int x = i % size;
-      int y = i / size;
-      cl::sycl::float4 Gx, Gy;
-        for(uint x_shift = 0; x_shift<5; x_shift++)
-             for(uint y_shift = 0; y_shift<5; y_shift++) {
-                  uint xs = x + x_shift - 2;
-                  uint ys = y + y_shift - 2;
-                  if(x==xs && y==ys)  continue;
-                  if(xs < 0 || xs >= size || ys < 0 || ys >= size) continue;
-                  cl::sycl::float4 sample = input[xs + ys * size];
-                  int offset_x  = x_shift + y_shift * radius;
-                  int offset_y  = y_shift + x_shift * radius;
-                  float conv_x   = kernel[offset_x];
-                  cl::sycl::float4 conv4_x = cl::sycl::float4(conv_x);
-                  Gx += conv4_x * sample;
-                  float conv_y   = kernel[offset_y];
-                  cl::sycl::float4 conv4_y = cl::sycl::float4(conv_y);
-                  Gy += conv4_y * sample;
-               }
-        cl::sycl::float4 color = hypot(Gx, Gy);
-        cl::sycl::float4 minval = cl::sycl::float4(0.0, 0.0, 0.0, 0.0);
-        cl::sycl::float4 maxval = cl::sycl::float4(1.0, 1.0, 1.0, 1.0);
-        cl::sycl::float4 expected = clamp(color, minval, maxval);
-        cl::sycl::float4 dif = fdim(output[i], expected);
-        float length = cl::sycl::length(dif);
-        if(length > 0.01f)
-        {
-            pass = false;
-            break;
+    QueueManager::getInstance().with_master_access([&](celerity::handler& cgh) {
+      auto result = output_buf.template get_access<cl::sycl::access::mode::read>(cgh, cl::sycl::range<2>(args.problem_size, args.problem_size));
+      
+      for (size_t i = 0; i < args.problem_size; i++) {
+        for (size_t j = 0; j < args.problem_size; j++) {
+          output[i*args.problem_size + j] = result[i][j];
         }
-    }
+      }
+
+      cgh.run([=, &pass]() {
+        save_bitmap("sobel5.bmp", size, output);
+
+        const float kernel[] = { 1, 2, 0,  -2, -1,4,  8, 0,  -8, -4, 6, 12, 0, -12, -6, 4,  8, 0,  -8, -4, 1,  2, 0,  -2, -1 };
+
+        bool pass = true;
+        int radius = 5;
+        for(size_t i=ver.begin[0]; i<ver.begin[0]+ver.range[0]; i++){
+          int x = i % size;
+          int y = i / size;
+          cl::sycl::float4 Gx, Gy;
+            for(uint x_shift = 0; x_shift<5; x_shift++)
+                for(uint y_shift = 0; y_shift<5; y_shift++) {
+                      uint xs = x + x_shift - 2;
+                      uint ys = y + y_shift - 2;
+                      if(x==xs && y==ys)  continue;
+                      if(xs < 0 || xs >= size || ys < 0 || ys >= size) continue;
+                      cl::sycl::float4 sample = input[xs + ys * size];
+                      int offset_x  = x_shift + y_shift * radius;
+                      int offset_y  = y_shift + x_shift * radius;
+                      float conv_x   = kernel[offset_x];
+                      cl::sycl::float4 conv4_x = cl::sycl::float4(conv_x);
+                      Gx += conv4_x * sample;
+                      float conv_y   = kernel[offset_y];
+                      cl::sycl::float4 conv4_y = cl::sycl::float4(conv_y);
+                      Gy += conv4_y * sample;
+                }
+            cl::sycl::float4 color = hypot(Gx, Gy);
+            cl::sycl::float4 minval = cl::sycl::float4(0.0, 0.0, 0.0, 0.0);
+            cl::sycl::float4 maxval = cl::sycl::float4(1.0, 1.0, 1.0, 1.0);
+            cl::sycl::float4 expected = clamp(color, minval, maxval);
+            cl::sycl::float4 dif = fdim(output[i], expected);
+            float length = cl::sycl::length(dif);
+            if(length > 0.01f)
+            {
+                pass = false;
+                break;
+            }
+        }
+      });
+    });
+    QueueManager::sync();
     return pass;
 }
 
