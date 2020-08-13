d9237d96982c44187434b57bc7e16b699ecfa5c9
Sohan Lal <sohan.lal@tu-berlin.de>
2020-08-13 13:52:03 +0200

    added other important combinations of two range mappers

--- micro/sequential_range_mappers.cc
+++ micro/sequential_range_mappers.cc
@@ -43,7 +43,7 @@ public:
     output_buf.initialize(output.data(), s::range<2>(args.problem_size, args.problem_size));
   }
 
-#if defined(BENCH_MAPPER_ONE_TO_ONE_ALL) 
+#if defined(BENCH_MAPPER_ONE_TO_ONE_ALL) || defined(BENCH_MAPPER_ALL_ONE_TO_ONE)
   void one_to_one(celerity::distr_queue& queue, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_a, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_b,celerity::buffer<BENCH_DATA_TYPE, 2>& buf_c) {
     queue.submit([=](celerity::handler& cgh) {
       auto a = buf_a.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::one_to_one<2>());
@@ -93,7 +93,6 @@ public:
       });
     });
   }
-
 
 #elif defined(BENCH_MAPPER_ONE_TO_ONE_SLICEX) 
   void one_to_one(celerity::distr_queue& queue, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_a, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_b,celerity::buffer<BENCH_DATA_TYPE, 2>& buf_c) {
@@ -145,8 +144,147 @@ public:
     });
   }
 
-#endif
+#elif defined(BENCH_MAPPER_ONE_TO_ONE_ONE_TO_ONE) 
+  void one_to_one(celerity::distr_queue& queue, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_a, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_b,celerity::buffer<BENCH_DATA_TYPE, 2>& buf_c) {
+    queue.submit([=](celerity::handler& cgh) {
+      auto a = buf_a.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::one_to_one<2>());
+      auto b = buf_b.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::one_to_one<2>());
+      auto c = buf_c.template get_access<cl::sycl::access::mode::discard_write>(cgh, celerity::access::one_to_one<2>());
 
+      cgh.parallel_for<class OneToOneMapperKernel>(cl::sycl::range<2>(args.problem_size, args.problem_size), [=](cl::sycl::item<2> item) {
+        c[{item[0], item[1]}] = a[{item[0], item[1]}] + b[{item[0], item[1]}];
+      });
+    });
+  }
+
+#elif defined(BENCH_MAPPER_ALL_ALL) 
+  void all(celerity::distr_queue& queue, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_a, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_b,celerity::buffer<BENCH_DATA_TYPE, 2>& buf_c) {
+    queue.submit([=](celerity::handler& cgh) {
+      auto a = buf_a.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::all<2>());
+      auto b = buf_b.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::all<2>());
+      auto c = buf_c.template get_access<cl::sycl::access::mode::discard_write>(cgh, celerity::access::one_to_one<2>());
+
+      cgh.parallel_for<class AllMapperKernel>(cl::sycl::range<2>(args.problem_size, args.problem_size), [=](cl::sycl::item<2> item) {
+        c[{item[0], item[1]}] = a[{item[0], item[1]}] + b[{item[0], item[1]}];
+      });
+    });
+  }
+
+#elif defined(BENCH_MAPPER_NEIGHBOURHOOD_NEIGHBOURHOOD) 
+ void neighborhood(celerity::distr_queue& queue, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_a, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_b,celerity::buffer<BENCH_DATA_TYPE, 2>& buf_c, size_t neigh_size) {
+    queue.submit([=](celerity::handler& cgh) {
+      auto a = buf_a.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::neighborhood<2>(neigh_size, neigh_size));
+      auto b = buf_b.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::neighborhood<2>(neigh_size, neigh_size));
+      auto c = buf_c.template get_access<cl::sycl::access::mode::discard_write>(cgh, celerity::access::one_to_one<2>());
+
+      cgh.parallel_for<class NeighborhoodMapperKernel>(cl::sycl::range<2>(args.problem_size, args.problem_size), [=](cl::sycl::item<2> item) {
+        c[{item[0], item[1]}] = a[{item[0], item[1]}] + b[{item[0], item[1]}];
+      });
+    });
+  } 
+
+#elif defined(BENCH_MAPPER_ALL_NEIGHBOURHOOD) 
+  void all(celerity::distr_queue& queue, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_a, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_b,celerity::buffer<BENCH_DATA_TYPE, 2>& buf_c) {
+    queue.submit([=](celerity::handler& cgh) {
+      auto a = buf_a.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::all<2>());
+      auto b = buf_b.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::all<2>());
+      auto c = buf_c.template get_access<cl::sycl::access::mode::discard_write>(cgh, celerity::access::one_to_one<2>());
+
+      cgh.parallel_for<class AllMapperKernel>(cl::sycl::range<2>(args.problem_size, args.problem_size), [=](cl::sycl::item<2> item) {
+        c[{item[0], item[1]}] = a[{item[0], item[1]}] + b[{item[0], item[1]}];
+      });
+    });
+  }
+
+  void neighborhood(celerity::distr_queue& queue, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_a, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_b,celerity::buffer<BENCH_DATA_TYPE, 2>& buf_c, size_t neigh_size) {
+    queue.submit([=](celerity::handler& cgh) {
+      auto a = buf_a.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::neighborhood<2>(neigh_size, neigh_size));
+      auto b = buf_b.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::neighborhood<2>(neigh_size, neigh_size));
+      auto c = buf_c.template get_access<cl::sycl::access::mode::discard_write>(cgh, celerity::access::one_to_one<2>());
+
+      cgh.parallel_for<class NeighborhoodMapperKernel>(cl::sycl::range<2>(args.problem_size, args.problem_size), [=](cl::sycl::item<2> item) {
+        c[{item[0], item[1]}] = a[{item[0], item[1]}] + b[{item[0], item[1]}];
+      });
+    });
+  }
+
+#elif defined(BENCH_MAPPER_ALL_SLICEX) 
+  void all(celerity::distr_queue& queue, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_a, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_b,celerity::buffer<BENCH_DATA_TYPE, 2>& buf_c) {
+    queue.submit([=](celerity::handler& cgh) {
+      auto a = buf_a.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::all<2>());
+      auto b = buf_b.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::all<2>());
+      auto c = buf_c.template get_access<cl::sycl::access::mode::discard_write>(cgh, celerity::access::one_to_one<2>());
+
+      cgh.parallel_for<class AllMapperKernel>(cl::sycl::range<2>(args.problem_size, args.problem_size), [=](cl::sycl::item<2> item) {
+        c[{item[0], item[1]}] = a[{item[0], item[1]}] + b[{item[0], item[1]}];
+      });
+    });
+  }
+
+  void slicex(celerity::distr_queue& queue, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_a, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_b,celerity::buffer<BENCH_DATA_TYPE, 2>& buf_c) {
+    queue.submit([=](celerity::handler& cgh) {
+      auto a = buf_a.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::slice<2>(0));
+      auto b = buf_b.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::slice<2>(0));
+      auto c = buf_c.template get_access<cl::sycl::access::mode::discard_write>(cgh, celerity::access::one_to_one<2>());
+
+      cgh.parallel_for<class SliceMapperKernelX>(cl::sycl::range<2>(args.problem_size, args.problem_size), [=](cl::sycl::item<2> item) {
+        c[{item[0], item[1]}] = a[{item[0], item[1]}] + b[{item[0], item[1]}];
+      });
+    });
+  } 
+
+#elif defined(BENCH_MAPPER_ALL_SLICEY) 
+  void all(celerity::distr_queue& queue, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_a, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_b,celerity::buffer<BENCH_DATA_TYPE, 2>& buf_c) {
+    queue.submit([=](celerity::handler& cgh) {
+      auto a = buf_a.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::all<2>());
+      auto b = buf_b.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::all<2>());
+      auto c = buf_c.template get_access<cl::sycl::access::mode::discard_write>(cgh, celerity::access::one_to_one<2>());
+
+      cgh.parallel_for<class AllMapperKernel>(cl::sycl::range<2>(args.problem_size, args.problem_size), [=](cl::sycl::item<2> item) {
+        c[{item[0], item[1]}] = a[{item[0], item[1]}] + b[{item[0], item[1]}];
+      });
+    });
+  }
+
+  void slicey(celerity::distr_queue& queue, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_a, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_b,celerity::buffer<BENCH_DATA_TYPE, 2>& buf_c) {
+    queue.submit([=](celerity::handler& cgh) {
+      auto a = buf_a.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::slice<2>(1));
+      auto b = buf_b.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::slice<2>(1));
+      auto c = buf_c.template get_access<cl::sycl::access::mode::discard_write>(cgh, celerity::access::one_to_one<2>());
+
+      cgh.parallel_for<class SliceMapperKernelX>(cl::sycl::range<2>(args.problem_size, args.problem_size), [=](cl::sycl::item<2> item) {
+        c[{item[0], item[1]}] = a[{item[0], item[1]}] + b[{item[0], item[1]}];
+      });
+    });
+  } 
+
+#elif defined(BENCH_MAPPER_SLICEY_SLICEX) || defined(BENCH_MAPPER_SLICEX_SLICEY)
+
+  void slicey(celerity::distr_queue& queue, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_a, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_b,celerity::buffer<BENCH_DATA_TYPE, 2>& buf_c) {
+    queue.submit([=](celerity::handler& cgh) {
+      auto a = buf_a.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::slice<2>(1));
+      auto b = buf_b.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::slice<2>(1));
+      auto c = buf_c.template get_access<cl::sycl::access::mode::discard_write>(cgh, celerity::access::one_to_one<2>());
+
+      cgh.parallel_for<class SliceMapperKernelX>(cl::sycl::range<2>(args.problem_size, args.problem_size), [=](cl::sycl::item<2> item) {
+        c[{item[0], item[1]}] = a[{item[0], item[1]}] + b[{item[0], item[1]}];
+      });
+    });
+  }
+
+  void slicex(celerity::distr_queue& queue, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_a, celerity::buffer<BENCH_DATA_TYPE, 2>& buf_b,celerity::buffer<BENCH_DATA_TYPE, 2>& buf_c) {
+    queue.submit([=](celerity::handler& cgh) {
+      auto a = buf_a.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::slice<2>(0));
+      auto b = buf_b.template get_access<cl::sycl::access::mode::read>(cgh, celerity::access::slice<2>(0));
+      auto c = buf_c.template get_access<cl::sycl::access::mode::discard_write>(cgh, celerity::access::one_to_one<2>());
+
+      cgh.parallel_for<class SliceMapperKernelX>(cl::sycl::range<2>(args.problem_size, args.problem_size), [=](cl::sycl::item<2> item) {
+        c[{item[0], item[1]}] = a[{item[0], item[1]}] + b[{item[0], item[1]}];
+      });
+    });
+  }
+
+#endif
 
   void run() {
 
@@ -158,6 +296,15 @@ public:
     // Matrix addition using all range mapper
     // a = b+c
     all(QueueManager::getInstance(), input2_buf.get(), output_buf.get(), input1_buf.get());
+    
+    #elif defined(BENCH_MAPPER_ALL_ONE_TO_ONE)
+    // Matrix addition using one_to_one range mapper
+    // c = a+b
+    all(QueueManager::getInstance(), input1_buf.get(), input2_buf.get(), output_buf.get());
+
+    // Matrix addition using all range mapper
+    // a = b+c
+    one_to_one(QueueManager::getInstance(), input2_buf.get(), output_buf.get(), input1_buf.get());    
     
     #elif defined(BENCH_MAPPER_ONE_TO_ONE_NEIGHBOURHOOD)
     // Matrix addition using one_to_one range mapper
@@ -185,7 +332,61 @@ public:
     // Matrix addition using slice range mapper
     // a = b+c
     slicey(QueueManager::getInstance(), input2_buf.get(), output_buf.get(), input1_buf.get());
-    #endif        
+
+    #elif defined(BENCH_MAPPER_ONE_TO_ONE_ONE_TO_ONE)
+    // Matrix addition using one_to_one range mapper
+    // c = a+b
+    one_to_one(QueueManager::getInstance(), input1_buf.get(), input2_buf.get(), output_buf.get());
+
+    // Matrix addition using one_to_one range mapper
+    // a = b+c
+    one_to_one(QueueManager::getInstance(), input2_buf.get(), output_buf.get(), input1_buf.get());
+   
+    #elif defined(BENCH_MAPPER_ALL_ALL)
+    // Matrix addition using all range mapper
+    // c = a+b
+    all(QueueManager::getInstance(), input1_buf.get(), input2_buf.get(), output_buf.get());
+
+    // Matrix addition using all range mapper
+    // a = b+c
+    all(QueueManager::getInstance(), input2_buf.get(), output_buf.get(), input1_buf.get());
+
+    #elif defined(BENCH_MAPPER_NEIGHBOURHOOD_NEIGHBOURHOOD)
+    // Matrix addition using neighbourhood range mapper
+    // c = a+b
+    neighborhood(QueueManager::getInstance(), input1_buf.get(), input2_buf.get(), output_buf.get(), 3);
+
+    // Matrix addition using neighbourhood range mapper
+    // a = b+c
+    neighborhood(QueueManager::getInstance(), input2_buf.get(), output_buf.get(), input1_buf.get(), 3);
+
+    #elif defined(BENCH_MAPPER_ALL_NEIGHBOURHOOD)
+    // Matrix addition using all range mapper
+    // c = a+b
+    all(QueueManager::getInstance(), input1_buf.get(), input2_buf.get(), output_buf.get());
+
+    // Matrix addition using neighbourhood range mapper
+    // a = b+c
+    neighborhood(QueueManager::getInstance(), input2_buf.get(), output_buf.get(), input1_buf.get(), 3);
+
+    #elif defined(BENCH_MAPPER_SLICEX_SLILCEY)
+    // Matrix addition using all range mapper
+    // c = a+b
+    slicex(QueueManager::getInstance(), input1_buf.get(), input2_buf.get(), output_buf.get());
+
+    // Matrix addition using all range mapper
+    // a = b+c
+    slicey(QueueManager::getInstance(), input2_buf.get(), output_buf.get(), input1_buf.get());
+
+    #elif defined(BENCH_MAPPER_SLICEY_SLILCEX)
+    // Matrix addition using all range mapper
+    // c = a+b
+    slicey(QueueManager::getInstance(), input1_buf.get(), input2_buf.get(), output_buf.get());
+
+    // Matrix addition using all range mapper
+    // a = b+c
+    slicex(QueueManager::getInstance(), input2_buf.get(), output_buf.get(), input1_buf.get());
+    #endif           
 
   // QueueManager::sync();  
   
